b3ef655aba99d69271fd55c9089e7f1847d0319f
Elena Afanaseva <af_el_val@mail.ru>
2022-04-30 02:22:04 +0400

    refactoring + full test coverage

--- contracts/Votings.sol
+++ contracts/Votings.sol
@@ -1,33 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-/// @title Voting.
+/// @title Votings.
 contract Votings {
     
     struct Candidate {                
+        uint id;
         uint votesCount; 
-        address payable candidateAddress;
-    }
-
-    struct Voter {         
-         bool voted;       
+        address payable candidateAddress;        
     }
 
     struct Voting {
         string name;        
         uint startTime;
         uint amount;
+        bool commissionWithdrawn;
         bool finished;
+        uint candidatesCount;
         Candidate[] candidates;
-        mapping(address => Voter) voters;
+        mapping(address => bool) voters;
     }
     
     uint public voteFee = 1e16 wei;
     uint commissionPercentage = 10;
 
     address public owner;
-    mapping(string => Voting) public votings;
-
+    mapping(string => Voting) private votings;
    
     constructor() {
         owner = msg.sender;
@@ -37,32 +35,26 @@ contract Votings {
         require( msg.sender == owner, "Only owner can create voting.");
 
         Voting storage voting = votings[name];
-        for (uint i = 0;i < candidateAddresses.length;i ++) {
-            voting.candidates.push(Candidate({votesCount: uint(0), candidateAddress: candidateAddresses[i]}));
+        for (uint i = 0;i < candidateAddresses.length;i ++) {            
+            voting.candidates.push(Candidate(i, uint(0), candidateAddresses[i]));
         }
-
+        voting.candidatesCount = candidateAddresses.length;
         voting.name = name;
-        voting.startTime = block.timestamp;        
+        voting.startTime = block.timestamp;
         voting.amount = 0;
     }
 
-    function vote(string calldata votingName, address candidateAddress) external payable {
+    function vote(string calldata votingName, uint candidateId) external payable {
         require (msg.value >= voteFee, "You don't have enough balance.");
         
         Voting storage voting = votings[votingName];
         require(voting.finished == false, "Voting was already finished.");
-
-        Voter storage sender = voting.voters[msg.sender];
-        require(sender.voted == false, "You already voted.");
-        
-        for (uint i = 0;i < voting.candidates.length;i ++) {
-            if (voting.candidates[i].candidateAddress == candidateAddress)
-            {
-                voting.candidates[i].votesCount ++;
-            }            
-        }
+        require(!voting.voters[msg.sender], "You already voted.");
+        require(candidateId < voting.candidatesCount, "Not correct candidate id.");
+       
+        voting.candidates[candidateId].votesCount +=1;
         voting.amount += msg.value;
-        voting.voters[msg.sender] = Voter({voted: true});        
+        voting.voters[msg.sender] = true;
     }
 
     function finish(string calldata votingName) external {
@@ -73,16 +65,20 @@ contract Votings {
         require(deadline <= block.timestamp, "Voting can be closed only 3 days after the start.");
 
         voting.finished = true;
+
+        address payable winner = payable(winningCandidate(votingName));
+        winner.transfer(voting.amount * (100 - commissionPercentage) / 100);
     }
 
     function withdrawCommission(string calldata votingName)  external {
         require( msg.sender == owner, "Only owner can withdraw commission.");
-        
+            
         Voting storage voting = votings[votingName];
         require(voting.finished == true, "Voting should be finished.");
-
-        address payable winner = payable(winningCandidate(votingName));
-        winner.transfer(voting.amount * commissionPercentage / 100);
+        require(!voting.commissionWithdrawn, "The commission has already been withdrawn.");
+           
+        payable(owner).transfer(voting.amount * commissionPercentage / 100);
+        voting.commissionWithdrawn = true;
     }
 
     function winningCandidate(string calldata votingName) public view
@@ -91,7 +87,7 @@ contract Votings {
         uint winningVoteCount = 0;
         Voting storage voting = votings[votingName];
 
-        for (uint p = 0; p < voting.candidates.length; p++) {
+        for (uint p = 0; p < voting.candidatesCount; p++) {
             if (voting.candidates[p].votesCount > winningVoteCount) {
                 winningVoteCount = voting.candidates[p].votesCount;
                 winningProposal_ = voting.candidates[p].candidateAddress;
@@ -99,11 +95,10 @@ contract Votings {
         }
     }
 
-    function viewVotings(string calldata votingName) public view
-            returns (Candidate[] memory candidates)
+    function viewVoting(string calldata votingName) public view
+            returns (string memory, Candidate[] memory, uint, bool, uint)
     {
         Voting storage voting = votings[votingName];
-        return voting.candidates;        
+        return (voting.name, voting.candidates, voting.startTime, voting.finished, voting.amount);
     }
-
 }
--- test/index.ts
+++ test/index.ts
@@ -6,16 +6,23 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 describe("Votings", function () {
   let contract: Contract;
   const votingName = "firstVoting";
-  
+
+  let owner: SignerWithAddress;
   let candidate1: SignerWithAddress;
   let candidate2: SignerWithAddress;
   let voter1: SignerWithAddress;
   let voter2: SignerWithAddress;
 
-  // const [candidate1, candidate2, voter1, voter2] = new MockProvider().getWallets()  
+  let name;
+  let candidates;
+  let startTime;
+  let finished;
+  let amount;
+
+  const voteFee = ethers.utils.parseEther('0.01');
 
   beforeEach(async () => {
-    [candidate1, candidate2, voter1, voter2] = await ethers.getSigners();  
+    [owner, candidate1, candidate2, voter1, voter2] = await ethers.getSigners();
 
     const Votings = await ethers.getContractFactory("Votings");
     contract = await Votings.deploy();
@@ -23,80 +30,163 @@ describe("Votings", function () {
   });
 
   describe("addVoting", function () {
+    it("Should return 2 candidates", async function () {
 
-    it("should return 2 candidates", async function () {            
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
-      
-      let candidates = await contract.viewVotings(votingName);  
+
+      [name, candidates, startTime, finished, amount] = await contract.viewVoting(votingName);
       expect(candidates[0].candidateAddress).to.equal(candidate1.address);
       expect(candidates[0].votesCount).to.equal(0);
-  
       expect(candidates[1].candidateAddress).to.equal(candidate2.address);
       expect(candidates[1].votesCount).to.equal(0);
     });
+
+    it("Should fail with error: Only owner can create voting. ", async function () {    
+      const tx = contract.connect(voter1).addVoting(votingName, [candidate1.address, candidate2.address]);
+
+      await expect(tx).to.be.revertedWith("Only owner can create voting.");
+    });
+
   });
-  
-  describe("vote", function () {    
-    it("should add candidates votes ", async function () {            
+
+  describe("vote", function () {
+    it("Should add candidates votes ", async function () {
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
 
-      await contract.connect(voter1).vote(votingName, candidate1.address, {
-        value: ethers.utils.parseEther('0.01'),
-      });
-        
-      let candidates = await contract.viewVotings(votingName);
-  
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+
+      [name, candidates, startTime, finished, amount] = await contract.viewVoting(votingName);
       expect(candidates[0].candidateAddress).to.equal(candidate1.address);
       expect(candidates[0].votesCount).to.equal(1);
-  
       expect(candidates[1].candidateAddress).to.equal(candidate2.address);
       expect(candidates[1].votesCount).to.equal(0);
     });
 
-    it("You already voted. ", async function () {            
+    it("Should fail with error: You already voted. ", async function () {
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
-      
-      await contract.connect(voter1).vote(votingName, candidate1.address, {
-        value: ethers.utils.parseEther('0.01'),
-      });      
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee });
 
-      const tx = contract.connect(voter1).vote(votingName, candidate1.address, {
-        value: ethers.utils.parseEther('0.01'),
-      });      
+      const tx = contract.connect(voter1).vote(votingName, 0, { value: voteFee });
+
       await expect(tx).to.be.revertedWith("You already voted.");
     });
 
-    it("You don't have enough balance. ", async function () {            
+    it("Should fail with error: You don't have enough balance. ", async function () {
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
-      
-      const voteTx = contract.connect(voter1).vote(votingName, candidate1.address, {
-        value: ethers.utils.parseEther('0.001'),
-      });            
+
+      const voteTx = contract.connect(voter1).vote(votingName, 0, { value: ethers.utils.parseEther('0.001') });
+
       await expect(voteTx).to.be.revertedWith("You don't have enough balance.");
     });
 
-    it("Voting finished.", async function () {            
+    it("Should fail with error: Voting was already finished.", async function () {
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
-      
       await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
-  
-      await contract.connect(voter1).finish(votingName);            
-      
-      const voteTx = contract.connect(voter1).vote(votingName, candidate1.address, {
-        value: ethers.utils.parseEther('0.01'),
-      });            
+      await contract.connect(voter1).finish(votingName);
+
+      const voteTx = contract.connect(voter1).vote(votingName, 0, { value: voteFee });
+
       await expect(voteTx).to.be.revertedWith("Voting was already finished.");
     });
-  }); 
-  
+
+    it("Should fail with error: Not correct candidate id. ", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+
+      const tx = contract.connect(voter1).vote(votingName, 10, { value: voteFee });
+
+      await expect(tx).to.be.revertedWith("Not correct candidate id.");
+    });
+  });
+
   describe("finish", function () {
 
-    it("Voting can be closed only 3 days after the start.", async function () {            
+    it("Should fail with error: Voting can be closed only 3 days after the start.", async function () {
       await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
-        
-      const finishTx = contract.connect(voter1).finish(votingName);            
+
+      const finishTx = contract.connect(voter1).finish(votingName);
+
       await expect(finishTx).to.be.revertedWith("Voting can be closed only 3 days after the start.");
-    });    
+    });
+
+    it("The reward must be transferred to the winner.", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+
+      const finishTx = contract.connect(voter1).finish(votingName);
+
+      await expect(await finishTx).to.changeEtherBalance(candidate1, ethers.utils.parseEther('0.009'));
+    });
+
+    it("Should fail with error: Voting was already finished.", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+      await contract.connect(voter1).finish(votingName);
+
+      const finishTx = contract.connect(voter1).finish(votingName);
+
+      await expect(finishTx).to.be.revertedWith("Voting was already finished.");
+    });
+
+  });
+
+  describe("winningCandidate", function () {
+
+    it("Should return first candidate ", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await contract.connect(voter2).vote(votingName, 0, { value: voteFee, });
+
+      let winnerAddres = await contract.winningCandidate(votingName);
+
+      expect(winnerAddres).to.equal(candidate1.address);
+    });
+  });
+
+  describe("withdrawCommission", function () {
+
+    it("Should withdraw commission to owner", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+      await contract.connect(voter1).finish(votingName);
+
+      const withdrawTx = contract.withdrawCommission(votingName);
+
+      await expect(await withdrawTx).to.changeEtherBalance(owner, ethers.utils.parseEther('0.001'));
+    });
+
+    it("Should fail with error: The commission has already been withdrawn.", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+      await contract.connect(voter1).finish(votingName);
+      await contract.withdrawCommission(votingName);
+
+      const withdrawTx = contract.withdrawCommission(votingName);
+
+      await expect(withdrawTx).to.be.revertedWith("The commission has already been withdrawn.");
+    });
+
+    it("Should fail with error: Voting should be finished.", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+
+      const withdrawTx = contract.withdrawCommission(votingName);
+
+      await expect(withdrawTx).to.be.revertedWith("Voting should be finished.");
+    });
+
+    it("Should fail with error: Only owner can withdraw commission.", async function () {
+      await contract.addVoting(votingName, [candidate1.address, candidate2.address]);
+      await contract.connect(voter1).vote(votingName, 0, { value: voteFee, });
+      await ethers.provider.send('evm_increaseTime', [3 * 24 * 60 * 60]);
+      await contract.connect(voter1).finish(votingName);
+
+      const withdrawTx = contract.connect(voter2).withdrawCommission(votingName);
+
+      await expect(withdrawTx).to.be.revertedWith("Only owner can withdraw commission.");
+    });
   });
 });
-
